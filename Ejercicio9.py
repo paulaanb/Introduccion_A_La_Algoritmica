35ff1f3c78f807e53a1cc1f7001cccf0de450629
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-02-18 21:35:09 +0100

    Finalizamos el ejercicio

--- Ejercicio9.py
+++ Ejercicio9.py
@@ -7,13 +7,39 @@ Entrada:
  N1, N2, N3: REAL 
  
 Precondición:
- n1, n2, n3 ≥ 0
+ N1, N2, N3 ≥ 0
 
 Realización:
- Resultado <-- (n1 + n2 + n3) / 3
+ Resultado <-- (N1 + N2 + n3) / 3
  
 Poscondición:
- Resultado = (n1 + n2 + n3) / 3
+ Resultado = (N1 + N2 + N3) / 3
  
 Fin del cálculo de la media aritmética
  
+Algoritmo: Media aritmética
+
+#Calculamos un algoritmo para calcular la media aritmética de tres números con sus respectivos coeficientes de ponderación
+
+Entrada:
+#Los números son N1, N2 y N3
+ N1, N2, N3: REAL 
+#Los coeficientes son C1, C2 y C3
+ C1, C2, C3: REAL 
+
+Precondición:
+ N1, N2, N3 ≥ 0
+
+ 0 < C1, C2, C3  ≤ 1 
+ 
+ 
+ Realización:
+ #Sumatorio del producto
+  Resultado <-- Σ(n*c)
+  
+ Poscondición:
+ #Sumatorio del producto
+  Resultado <-- Σ(n*c)
+  
+ Fin del cálculo de la media arirmética con los coeficientes de ponderación
+  
