5dc53c476ed6d4e3fa4bc209d4cde625af30d517
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-02-18 21:29:14 +0100

    Primera parte segundo ejercicio terminado

---
+++ Ejercicio9.py
@@ -0,0 +1,19 @@
+Algoritmo: Media arimética
+
+#Calculamos un algoritmo para calcular la media aritmética de tres números dados
+
+Entrada:
+#Los números dados son N1, N2 y N3
+ N1, N2, N3: REAL 
+ 
+Precondición:
+ n1, n2, n3 ≥ 0
+
+Realización:
+ Resultado <-- (n1 + n2 + n3) / 3
+ 
+Poscondición:
+ Resultado = (n1 + n2 + n3) / 3
+ 
+Fin del cálculo de la media aritmética
+ 
