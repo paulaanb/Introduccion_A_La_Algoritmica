fcc398c075d8b21507d356314032e6f002428d0f
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-02-18 21:47:09 +0100

    Terminado tambien

---
+++ Ejercicio10.py
@@ -0,0 +1,22 @@
+Algoritmo: Área del triángulo
+
+#Calculamos el área del tríangulo dados un lado y la altura de este
+
+Entrada:
+#Lado del triángulo
+ l:REAL 
+ #Altura del lado
+ h: REAL
+ 
+Precondición:
+ l > 0
+ h > 0
+
+Realización:
+ Resultado <-- (l*h)/2
+ 
+Poscondición:
+ Resultado = (l*h)/2
+ 
+Fin de calcular el área del triángulo
+ 
